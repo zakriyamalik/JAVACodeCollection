d37e17ad87decab86e9f341de9e324c8d2f6ab44
Muhammad Zakriya <zakriyamalik852@gmail.com>
2024-09-06 14:22:59 +0500

    Pata nai 1

--- LESCO billing system/src/Main.java
+++ LESCO billing system/src/Main.java
@@ -2,20 +2,26 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.*;
+
 //import ;
 
 public class Main {
-    public void menu(int choice)
+    public static int menu(int choice)
     {
-
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter 1 to Create File \nEnter 2 to Write file\nEnter 2 to Read file");
-
+         choice = scanner.nextInt();
+        return choice;
     }
     public static void main(String[] args) {
-        int choice=0;
-        //choice=menu(choice);
-
         System.out.printf("Hello and welcome!");
+        int choice=0;
+        choice=menu(choice);
+        System.out.printf("Choice is :"+choice);
+
+
+       
 
         try {
             File file = new File("EmployeesData.txt");
